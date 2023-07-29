24b9064c631f35b4cd2e8e9070112228278014ee
ELenS23 <serbinalen@mail.ru>
2023-07-29 17:43:20 +0300

    Add hangman game files

---
+++ src/js/utils.js
@@ -0,0 +1,21 @@
+export const darkModeHandle = () => {
+
+    const darkModelSwitcher = document.getElementById('toggleDarkMode');
+    const htmlElement = document.documentElement;
+
+    if (localStorage.getItem('mode') === 'dark') {
+        htmlElement.classList.add('dark')
+        darkModelSwitcher.checked = true
+    }
+
+    darkModelSwitcher.addEventListener('input', () => {
+        const htmlElement = document.documentElement;
+        htmlElement.classList.toggle('dark');
+
+        if (htmlElement.classList.contains('dark')) {
+            localStorage.setItem('mode', 'dark')
+        } else {
+            localStorage.setItem('mode', 'light')
+        }
+    });
+};
